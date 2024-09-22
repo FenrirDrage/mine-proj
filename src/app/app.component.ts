76514b8d9859cfded040fb88be4cf32f1f90d343
Sérgio Alves <garfield.alves@gmail.com>
2024-09-22 13:45:25 +0200

    add boostrap to project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MyloginComponent } from './components/mylogin/mylogin.component';
+import { CtableComponent } from './components/ctable/ctable.component';
+import { DefmenuComponent } from './components/defmenu/defmenu.component';
+import { MenubarComponent } from './components/menubar/menubar.component';
+import { SubmenuComponent } from './components/submenu/submenu.component';
+import { TopheaderComponent } from './components/topheader/topheader.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MyloginComponent],
+  imports: [RouterOutlet, MyloginComponent, CtableComponent, DefmenuComponent, MenubarComponent,SubmenuComponent,TopheaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
